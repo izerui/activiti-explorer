5be4999f19c3bd248c4833892b6889c8731d5d48
myfeike <liuyuhua69@gmail.com>
2013-11-08 22:49:01 +0800

    1. 更新到5.14 2. 整合rest 3. 加入rest 测试

---
+++ src/test/java/test/rest/TestRest.java
@@ -0,0 +1,53 @@
+package test.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+
+/**
+ * 请先启动服务器 启动命令: mvn jetty:run
+ * @author izerui.com
+ * @version createtime：2013年8月3日 下午10:22:52
+ */
+public class TestRest {
+	@Test
+	public void test() throws Exception {
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope("localhost", 8080), new UsernamePasswordCredentials("kermit",
+				"kermit"));
+		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+		try {
+			HttpGet httpget = new HttpGet(
+					"http://localhost:8080/activiti-explorer/service/repository/process-definitions");
+			System.out.println("executing request" + httpget.getRequestLine());
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			try {
+				writeMessage(response);
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+	}
+
+	public void writeMessage(HttpResponse response) throws IllegalStateException, IOException {
+		System.out.println("results: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+		String output;
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+	}
+}
